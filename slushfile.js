f542110048fd294748c64c493b89e23835f99547
Ben Teichman <ben.l.teichman@gmail.com>
2017-07-27 15:56:10 -0400

    fix(template): use match() not test()

--- slushfile.js
+++ slushfile.js
@@ -50,14 +50,15 @@ gulp.task('default', function(done) {
       gulp.src(path.join(__dirname, 'templates', '**', '*'))
         .pipe(template(answers, { interpolate: /<%=([\s\S]+?)%>/g }))
         .pipe(rename(function(file) {
-          if (file.dirname.test(/%component/)) {
+          if (file.dirname.match(/%component/)) {
             file.dirname = file.dirname.replace('%component', answers.component);
           } else if (file.basename === '%component') {
             file.basename = answers.component;
           }
-          if (file.dirname.test(/\/_/)) {
+          if (file.dirname.match(/\/_/)) {
             file.dirname = file.dirname.replace('/_', '/.');
-          } else if (file.basename[0] === '_') {
+          }
+          if (file.basename[0] === '_') {
             file.basename = '.' + file.basename.slice(1);
           }
           if (file.basename[0] === '-') {
